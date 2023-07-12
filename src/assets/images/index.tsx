58e9296904a8136c5d2aebc3c1c3f9aecff951d5
Aiden <bokjaeoh@gmail.com>
2023-07-12 15:42:02 +0900

    푸터 영역 구성 및 스타일 적용

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,5 +1,6 @@
 import Logo from './logo.png'
+import LogoBottom from './logo_bottom.png'
 
-const images = { Logo }
+const images = { Logo, LogoBottom }
 
 export default images
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,17 @@
 import { Container } from '@mui/material'
+import { Typography as T } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 
+import IMGS from '../assets/images'
+
 const Footer = () => {
-  return <Container className="Footer">Footer</Container>
+  return (
+    <Container className="Footer">
+      <Image src={IMGS.LogoBottom} alt="EONE Logo" />
+      <T>COPYRIGHT(C) 2023 EONE HEALTHCARE. ALL RIGHTS RESERVED.</T>
+    </Container>
+  )
 }
 
 export default Footer
