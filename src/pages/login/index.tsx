b82ddd4d5ee8c154f171417b1f8c63ba8fb25004
Aiden <bokjaeoh@gmail.com>
2023-07-13 17:06:56 +0900

    로그인 페이지 레이아웃 구성 및 스타일 적용

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,25 @@
-import { Container } from '@mui/material'
+import { Box, Container, Typography as T } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 
-const LoginPage = () => {
-  const className = 'Pages HomePage'
+import Footer from '../../components/Footer'
+import IMGS from '../../assets/images'
 
-  return <Container className={className}>LoginPage</Container>
+const LoginPage = () => {
+  const className = 'Pages LoginPage'
+
+  return (
+    <Container className={className}>
+      <Box className="Logo">
+        <Image src={IMGS.Logo} alt="Logo" />
+      </Box>
+      <Box className="LoginBox">
+        <T component="h1">MOBILE CONSENT SYSTEM</T>
+        <T component="h2">모바일 전자동의서 시스템</T>
+      </Box>
+      <Footer />
+    </Container>
+  )
 }
 
 export default LoginPage
