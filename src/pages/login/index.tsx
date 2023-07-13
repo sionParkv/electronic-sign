f4d9ed4fbb7546fecb5306269b9b2a312169f524
parksion <so.park@4cgate.com>
2023-07-13 19:24:14 +0900

    로그인 레이아웃 및 스타일 적용

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,10 @@
-import { Box, Container, Typography as T } from '@mui/material'
+import {
+  Box,
+  Button,
+  Container,
+  Typography as T,
+  TextField
+} from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 
@@ -16,6 +22,31 @@ const LoginPage = () => {
       <Box className="LoginBox">
         <T component="h1">MOBILE CONSENT SYSTEM</T>
         <T component="h2">모바일 전자동의서 시스템</T>
+        <Box className="LoginText">
+          <Box>
+            <T>사번</T>
+            <T>|</T>
+            <TextField />
+          </Box>
+          <Box>
+            <T>이름</T>
+            <T>|</T>
+            <TextField />
+          </Box>
+          <Box>
+            <T>부서</T>
+            <T>|</T>
+            <TextField />
+          </Box>
+          <Box>
+            <T>비밀번호</T>
+            <T>|</T>
+            <TextField type="password" />
+          </Box>
+        </Box>
+        <Box className="LoginBtn">
+          <Button>로그인</Button>
+        </Box>
       </Box>
       <Footer />
     </Container>
