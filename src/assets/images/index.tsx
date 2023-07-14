dab1dbc5f28d2203a820db6ad0d436a41d954718
parksion <so.park@4cgate.com>
2023-07-14 17:13:34 +0900

    헤더 메뉴바 클릭 이벤트 생성

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -2,7 +2,8 @@ import Logo from './logo.png'
 import LogoBottom from './logo_bottom.png'
 import PatientIcon from './ic_patient.png'
 import DocumentIcon from './ic_document.png'
+import WhiteLogo from './logo_white.png'
 
-const images = { Logo, LogoBottom, PatientIcon, DocumentIcon }
+const images = { Logo, LogoBottom, PatientIcon, DocumentIcon, WhiteLogo }
 
 export default images
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,38 @@
 import MenuIcon from '@mui/icons-material/Menu'
-import { AppBar, Button, Toolbar, Typography as T } from '@mui/material'
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography as T,
+  Drawer,
+  ListItem,
+  ListItemText
+} from '@mui/material'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 import IMGS from '../assets/images'
 
 const Header = (props: { userInfo: string }) => {
   const { userInfo } = props
+  const [open, setOpen] = useState(false)
+
+  const data = [
+    { name: '모바일 전자 동의서' },
+    { name: 'Inbox' },
+    { name: 'Outbox' }
+  ]
+
+  const getList = () => (
+    <div onClick={() => setOpen(false)} className="DrawerMenu">
+      <Image src={IMGS.WhiteLogo} alt="Logo" />
+      {data.map((item, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={item.name} />
+        </ListItem>
+      ))}
+    </div>
+  )
 
   return (
     <AppBar className="Header">
@@ -16,9 +42,12 @@ const Header = (props: { userInfo: string }) => {
         </Button>
         <T>{userInfo} 님</T>
         <Button className="Logout">로그아웃</Button>
-        <Button className="Menu">
+        <Button className="Menu" onClick={() => setOpen(true)}>
           <MenuIcon />
         </Button>
+        <Drawer open={open} anchor={'right'} onClose={() => setOpen(false)}>
+          {getList()}
+        </Drawer>
       </Toolbar>
     </AppBar>
   )
