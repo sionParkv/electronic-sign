04757521093724f5469acdc89c3c884c283e828e
parksion <so.park@4cgate.com>
2023-07-19 21:23:17 +0900

    목록 버튼 추가 및 문서 영역만 스크롤 되도록 수정

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Container,
   List,
   ListItem,
@@ -48,137 +49,142 @@ const Document = () => {
 
   return (
     <Container className="DocumentContainer">
-      <Box className="DocumentBox">
-        <Box className="DocumentTitle">
-          <Image src={images.DocumentIcon} alt="icon" />
-          <T>작성서식 목록</T>
-        </Box>
-        <Box className="TabContainer">
-          <Box>
-            <Tabs value={tabL} onChange={handleChangeL}>
-              <Tab label="임시저장 서식" />
-              <Tab label="작성완료 서식" />
-            </Tabs>
+      <Box className="Division">
+        <Box className="DocumentBox">
+          <Box className="DocumentTitle">
+            <Image src={images.DocumentIcon} alt="icon" />
+            <T>작성서식 목록</T>
           </Box>
-          <DocumentListTabPanel value={tabL} index={0}>
-            <DocumentListContainer>
-              <List className="DocumentList">
-                <ListItem>
-                  <T className="Title">표준 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-              </List>
-            </DocumentListContainer>
-          </DocumentListTabPanel>
-          <DocumentListTabPanel value={tabL} index={1}>
-            <DocumentListContainer>
-              <List className="DocumentList">
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-                <ListItem>
-                  <T className="Title">완료된 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-              </List>
-            </DocumentListContainer>
-          </DocumentListTabPanel>
+          <Box className="TabContainer">
+            <Box>
+              <Tabs value={tabL} onChange={handleChangeL}>
+                <Tab label="임시저장 서식" />
+                <Tab label="작성완료 서식" />
+              </Tabs>
+            </Box>
+            <DocumentListTabPanel value={tabL} index={0}>
+              <DocumentListContainer>
+                <List className="DocumentList">
+                  <ListItem>
+                    <T className="Title">표준 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                </List>
+              </DocumentListContainer>
+            </DocumentListTabPanel>
+            <DocumentListTabPanel value={tabL} index={1}>
+              <DocumentListContainer>
+                <List className="DocumentList">
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                  <ListItem>
+                    <T className="Title">완료된 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                </List>
+              </DocumentListContainer>
+            </DocumentListTabPanel>
+          </Box>
+        </Box>
+        <Box className="DocumentBox">
+          <Box className="DocumentTitle">
+            <Image src={images.DocumentIcon} alt="icon" />
+            <T>서식 목록</T>
+          </Box>
+          <Box className="TabContainer">
+            <Box>
+              <Tabs value={tabR} onChange={handleChangeR}>
+                <Tab label="즐겨찾기" />
+                <Tab label="전체" />
+              </Tabs>
+            </Box>
+            <DocumentListTabPanel value={tabR} index={0}>
+              <DocumentListContainer>
+                <List className="DocumentList">
+                  <ListItem>
+                    <T className="Title">즐겨찾기 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                </List>
+              </DocumentListContainer>
+            </DocumentListTabPanel>
+            <DocumentListTabPanel value={tabR} index={1}>
+              <DocumentListContainer>
+                <List className="DocumentList">
+                  <ListItem>
+                    <T className="Title">일반 공통 수술시술검사 동의서</T>
+                    <T className="Date">2023.06.14 12:56</T>
+                  </ListItem>
+                </List>
+              </DocumentListContainer>
+            </DocumentListTabPanel>
+          </Box>
         </Box>
       </Box>
-      <Box className="DocumentBox">
-        <Box className="DocumentTitle">
-          <Image src={images.DocumentIcon} alt="icon" />
-          <T>서식 목록</T>
-        </Box>
-        <Box className="TabContainer">
-          <Box>
-            <Tabs value={tabR} onChange={handleChangeR}>
-              <Tab label="즐겨찾기" />
-              <Tab label="전체" />
-            </Tabs>
-          </Box>
-          <DocumentListTabPanel value={tabR} index={0}>
-            <DocumentListContainer>
-              <List className="DocumentList">
-                <ListItem>
-                  <T className="Title">즐겨찾기 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-              </List>
-            </DocumentListContainer>
-          </DocumentListTabPanel>
-          <DocumentListTabPanel value={tabR} index={1}>
-            <DocumentListContainer>
-              <List className="DocumentList">
-                <ListItem>
-                  <T className="Title">일반 공통 수술시술검사 동의서</T>
-                  <T className="Date">2023.06.14 12:56</T>
-                </ListItem>
-              </List>
-            </DocumentListContainer>
-          </DocumentListTabPanel>
-        </Box>
+      <Box className="ButtonBox">
+        <Button className="list">목록</Button>
       </Box>
     </Container>
   )
