3a580ae19f79e2018189e12cb5b10d846745b1b2
nh.lee <nh.lee@4cgate.com>
2023-08-25 13:17:28 +0900

    로그인 중복 코드 정리

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -14,51 +14,69 @@ import IMGS from '../../assets/images'
 import { useRouter } from 'next/router'
 import { hasCookie } from 'cookies-next'
 
+interface UserInfo {
+  name: string
+  dept: string
+}
+
+/**
+ * 로그인 페이지 컴포넌트
+ * @returns {React.ReactNode} 로그인 페이지 컴포넌트
+ */
 const LoginPage = () => {
-  const [empNo, setEmpNo] = useState('')
-  const [pw, setPw] = useState('')
+  const [empNo, setEmpNo] = useState('') // 사용자 사번 상태
+  const [pw, setPw] = useState('') // 사용자 비밀번호 상태
   const className = 'Pages LoginPage'
   const router = useRouter()
-  const [userInfo, setUserInfo] = useState({
+  const [userInfo, setUserInfo] = useState<UserInfo>({
     name: '',
     dept: ''
-  })
+  }) // 사용자 정보 상태  (이름, 부서)
 
+  // 이미 로그인된 사용자는 홈 페이지로 리다이렉트
   if (hasCookie('loginCookie')) {
     router.push('/')
   }
 
-  const handleChangePw = (e: any) => {
-    const pwValue = e.target.value
-    setPw(pwValue)
+  const openErrorDialog = (contents: React.ReactNode) => {
+    components.openConfirmDialog({
+      contents,
+      ok: {
+        label: '닫기',
+        action: () => {
+          setTimeout(() => {
+            document.getElementsByTagName('input')[0].focus()
+          }, 50)
+        }
+      },
+      title: '입력 오류'
+    })
+    return
   }
 
-  const handleEmpNo = (e: React.FocusEvent<HTMLInputElement>) => {
-    const EMPL_NO = e.target.value
-    setEmpNo(EMPL_NO)
-
-    if (!EMPL_NO) return
-    if (EMPL_NO !== empNo) {
-      axios
-        .post('/api/login', {
-          EMPL_NO,
-          PASS_WORD: pw,
-          CODE: 'EMPL_NO_CHECK'
-        })
-        .then((response) => {
-          const result = response.data
-          if (result.code === 'OK') {
-            const data = result.data[0]
-            setUserInfo({
-              name: data.EMPL_NM,
-              dept: data.DEPT_GB
-            })
-            return
-          } else if (result.code === 'FAIL') {
-            setUserInfo({ name: '', dept: '' })
-            setPw('')
+  const axiosRequest = (emplNo: string, passWord: string, code: string) => {
+    axios
+      .post('/api/login', {
+        EMPL_NO: emplNo,
+        PASS_WORD: passWord,
+        CODE: code
+      })
+      .then((response) => {
+        const result = response.data
+        if (code === 'LOGIN') {
+          if (response.data.code === 'OK') {
+            // 로그인 성공 시 홈 페이지로 이동하고 탭 초기화
+            router.push('/')
+            localStorage.setItem('newTab', '0')
+          } else {
+            // 로그인 실패 시 오류 메시지 표시
             components.openConfirmDialog({
-              contents: result.message,
+              contents: (
+                <>
+                  입력한 결과와 일치하는 <br />
+                  직원 정보가 없습니다.
+                </>
+              ),
               ok: {
                 label: '닫기',
                 action: () => {
@@ -69,94 +87,64 @@ const LoginPage = () => {
               },
               title: '입력 오류'
             })
-            return
           }
-        })
-        .catch(() => {
-          components.openConfirmDialog({
-            contents: (
-              <>
-                전산상의 오류가 발생했습니다. <br /> 다음에 다시 시도해주세요.
-              </>
-            ),
-            ok: {
-              label: '닫기',
-              action: () => {
-                setTimeout(() => {
-                  document.getElementsByTagName('input')[0].focus()
-                }, 50)
-              }
-            },
-            title: '입력 오류'
-          })
-          return
-        })
-    }
-  }
-
-  const handleLogin = () => {
-    if (!empNo.length) {
-      return components.openConfirmDialog({
-        contents: '사번을 입력해주세요',
-        ok: {
-          label: '닫기',
-          action: () => {
-            setTimeout(() => {
-              document.getElementsByTagName('input')[0].focus()
-            }, 50)
+        } else if (code === 'EMPL_NO_CHECK') {
+          if (result.code === 'OK') {
+            const data = result.data[0]
+            setUserInfo({
+              name: data.EMPL_NM,
+              dept: data.DEPT_GB
+            }) // 사용자 정보 업데이트
+          } else if (result.code === 'FAIL') {
+            setUserInfo({ name: '', dept: '' })
+            setPw('')
+            openErrorDialog(result.message)
           }
-        },
-        title: '입력 오류'
-      })
-    } else if (!pw.length) {
-      return components.openConfirmDialog({
-        contents: '비밀번호를 입력해주세요',
-        ok: {
-          label: '닫기',
-          action: () => {
-            setTimeout(() => {
-              document.getElementsByTagName('input')[3].focus()
-            }, 50)
-          }
-        },
-        title: '입력 오류'
-      })
-    }
-    axios
-      .post('/api/login', {
-        EMPL_NO: empNo,
-        PASS_WORD: pw,
-        CODE: 'LOGIN'
-      })
-      .then((response) => {
-        if (response.data.code === 'OK') {
-          router.push('/')
-          localStorage.setItem('newTab', '0')
-        } else {
-          components.openConfirmDialog({
-            contents: (
-              <>
-                입력한 결과와 일치하는 <br />
-                직원 정보가 없습니다.
-              </>
-            ),
-            ok: {
-              label: '닫기',
-              action: () => {
-                setTimeout(() => {
-                  document.getElementsByTagName('input')[0].focus()
-                }, 50)
-              }
-            },
-            title: '입력 오류'
-          })
         }
       })
-      .catch((error) => {
-        alert(error.message)
+      .catch(() => {
+        openErrorDialog(
+          <>
+            전산상의 오류가 발생했습니다. <br /> 다음에 다시 시도해주세요.
+          </>
+        )
       })
   }
 
+  // 비밀번호 변경 핸들러
+  const handleChangePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pwValue = e.target.value
+    setPw(pwValue)
+  }
+
+  // 사번 입력 핸들러
+  const handleEmpNo = (e: React.FocusEvent<HTMLInputElement>) => {
+    const EMPL_NO = e.target.value
+    setEmpNo(EMPL_NO)
+
+    if (!EMPL_NO) return
+
+    if (EMPL_NO !== empNo) {
+      axiosRequest(EMPL_NO, pw, 'EMPL_NO_CHECK')
+    }
+  }
+
+  // 로그인 버튼 클릭 핸들러
+  const handleLogin = () => {
+    if (!empNo.length) {
+      // 사번이 입력되지 않은 경우 오류 메시지 표시
+      openErrorDialog('사번을 입력해주세요')
+      return
+    } else if (!pw.length) {
+      // 비밀번호가 입력되지 않은 경우 오류 메시지 표시
+      openErrorDialog('비밀번호를 입력해주세요')
+      return
+    } else {
+      axiosRequest(empNo, pw, 'LOGIN')
+    }
+  }
+
+  // 컴포넌트 반환
   return (
     <Container className={className}>
       <Box className="Logo">
