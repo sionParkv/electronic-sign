d97f2c9a2ea70538afe122c79a8d81706b1ba4d0
nh.lee <nh.lee@4cgate.com>
2023-08-28 17:13:13 +0900

    로그인 화면 랜더링 전 로딩바 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,42 +1,52 @@
 import {
   Box,
   Button,
+  CircularProgress,
   Container,
   Typography as T,
   TextField
 } from '@mui/material'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import components from '@/components'
 import IMGS from '../../assets/images'
 import { useRouter } from 'next/router'
-import { hasCookie } from 'cookies-next'
+// import { hasCookie } from 'cookies-next'
 
 interface UserInfo {
   name: string
   dept: string
 }
 
+interface LoginPageProps {
+  isLoggedIn: boolean
+}
+
 /**
  * 로그인 페이지 컴포넌트
  * @returns {React.ReactNode} 로그인 페이지 컴포넌트
  */
-const LoginPage = () => {
+const LoginPage: React.FC<LoginPageProps> = ({ isLoggedIn }) => {
   const [empNo, setEmpNo] = useState('') // 사용자 사번 상태
   const [pw, setPw] = useState('') // 사용자 비밀번호 상태
   const className = 'Pages LoginPage'
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState<boolean>(true) // 로딩 상태 관리
   const [userInfo, setUserInfo] = useState<UserInfo>({
     name: '',
     dept: ''
   }) // 사용자 정보 상태  (이름, 부서)
 
-  // 이미 로그인된 사용자는 홈 페이지로 리다이렉트
-  if (hasCookie('loginCookie')) {
-    router.push('/')
-  }
+  useEffect(() => {
+    // 이미 로그인된 사용자는 홈 페이지로 리다이렉트
+    if (isLoggedIn) {
+      router.push('/')
+    } else {
+      setIsLoading(false)
+    }
+  }, [])
 
   const openErrorDialog = (contents: React.ReactNode) => {
     components.openConfirmDialog({
@@ -147,56 +157,74 @@ const LoginPage = () => {
   // 컴포넌트 반환
   return (
     <Container className={className}>
-      <Box className="Logo">
-        <Image src={IMGS.Logo} alt="Logo" width="143" height="30" />
-      </Box>
-      <Box className="LoginBox">
-        <T component="h1">MOBILE CONSENT SYSTEM</T>
-        <T component="h2">모바일 전자동의서 시스템</T>
-        <Box className="LoginText" component="form">
-          <Box>
-            <T>사번</T>
-            <TextField
-              defaultValue={empNo}
-              onBlur={handleEmpNo}
-              placeholder="사번 입력"
-              type="number"
-            />
-          </Box>
-          <Box className="Name">
-            <T>이름</T>
-            <TextField
-              InputProps={{ readOnly: true }}
-              placeholder="사번 일치 시 자동 입력"
-              value={userInfo.name}
-            />
-          </Box>
-          <Box className="Dept">
-            <T>부서</T>
-            <TextField
-              InputProps={{ readOnly: true }}
-              placeholder="사번 일치 시 자동 입력"
-              value={userInfo.dept}
-            />
-          </Box>
-          <Box>
-            <T>비밀번호</T>
-            <TextField
-              type="password"
-              value={pw}
-              placeholder="비밀번호 입력"
-              onChange={handleChangePw}
-              autoComplete="off"
-            />
-          </Box>
+      {isLoading ? (
+        <Box className="Progress">
+          <CircularProgress />
         </Box>
-        <Box className="LoginBtn">
-          <Button onClick={handleLogin}>로그인</Button>
-        </Box>
-      </Box>
-      <components.Footer />
+      ) : (
+        <>
+          <Box className="Logo">
+            <Image src={IMGS.Logo} alt="Logo" width="143" height="30" />
+          </Box>
+          <Box className="LoginBox">
+            <T component="h1">MOBILE CONSENT SYSTEM</T>
+            <T component="h2">모바일 전자동의서 시스템</T>
+            <Box className="LoginText" component="form">
+              <Box>
+                <T>사번</T>
+                <TextField
+                  defaultValue={empNo}
+                  onBlur={handleEmpNo}
+                  placeholder="사번 입력"
+                  type="number"
+                />
+              </Box>
+              <Box className="Name">
+                <T>이름</T>
+                <TextField
+                  InputProps={{ readOnly: true }}
+                  placeholder="사번 일치 시 자동 입력"
+                  value={userInfo.name}
+                />
+              </Box>
+              <Box className="Dept">
+                <T>부서</T>
+                <TextField
+                  InputProps={{ readOnly: true }}
+                  placeholder="사번 일치 시 자동 입력"
+                  value={userInfo.dept}
+                />
+              </Box>
+              <Box>
+                <T>비밀번호</T>
+                <TextField
+                  type="password"
+                  value={pw}
+                  placeholder="비밀번호 입력"
+                  onChange={handleChangePw}
+                  autoComplete="off"
+                />
+              </Box>
+            </Box>
+            <Box className="LoginBtn">
+              <Button onClick={handleLogin}>로그인</Button>
+            </Box>
+          </Box>
+          <components.Footer />
+        </>
+      )}
     </Container>
   )
 }
 
 export default LoginPage
+
+export async function getServerSideProps(context: any) {
+  const cookies = context.req.cookies
+  const isLoggedIn = cookies.loginCookie ? true : false
+  return {
+    props: {
+      isLoggedIn
+    }
+  }
+}
