7fdc1aeb7d59edb2cde23ca3ddfa482fe90bc182
sh.yang <sh.yang@4cgate.com>
2023-09-01 09:40:43 +0900

    FTP 파일 업로드 위치 수정

--- src/pages/api/completeSave.tsx
+++ src/pages/api/completeSave.tsx
@@ -31,18 +31,15 @@ const upload = (fileName: any, filePath: string, PTNT_NO: string) =>
       .then(async (response) => {
         try {
           logger.debug('FTP Client connection response: %o', response)
+          const targetFolder = `/EFORM01/${moment().format(
+            'YYYY'
+          )}/${moment().format('MM')}/${moment().format(
+            'DD'
+          )}/${PTNT_NO.padStart(9, '0')}`
+          logger.debug('원격 파일 저장 경로 ' + targetFolder)
+          await client.ensureDir(targetFolder) //업로드 할 폴더 유무 확인 및 생성
           response = await client.cd('/EFORM01') // 서버에 접속 후, 업로드할 폴더로 이동
           logger.debug('FTP Client change directory response: %o', response)
-          const targetFolder = `/${moment().format('YYYY')}/${moment().format(
-            'MM'
-          )}/${moment().format('DD')}/${PTNT_NO.padStart(9, '0')}`
-          logger.debug('원격 파일 저장 경로 ' + targetFolder)
-          let result: any = await client.ensureDir(
-            `/${moment().format('YYYY')}`
-          )
-          logger.debug('FTP Client Create Directory : %o', result)
-          response = await client.cd(`/${moment().format('YYYY')}`)
-          logger.debug('FTP Client change2 directory response: %o', response)
           response = await client.uploadFrom(filePath, fileName)
           logger.debug('FTP Client file upload response: %o', response)
           resolve(true)
